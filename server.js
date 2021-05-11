a660ddc8768323db408997b93fee528a7cbed39d
aromvergara <aromvergara@gmail.com>
2021-05-10 20:56:01 -0500

    final

--- server.js
+++ server.js
@@ -23,10 +23,15 @@ const quotesCollection = db.collection('quotes');
   app.use(bodyParser.json())
   app.use(express.static('public'))
 
- 
-  app.listen(3000, function() {
-      console.log('listening on 3000')
+  app.set('port', process.env.PORT || 3000);
+  app.listen(app.get('port'), function() {
+      console.log('listening on 3000'),
+      console.log(app.get('port'))
   })
+
+//   app.listen(3000, function() {
+//     console.log('listening on 3000')
+// })
 
   app.post('/quotes', (req,res) => {
     collection.insertOne(req.body)
